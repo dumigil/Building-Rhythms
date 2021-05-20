bb852a1bf40e1a54be3f608808e99a9182536c64
Michiel de Jong <m.d.dejong-1@student.tudelft.nl>
2021-05-20 23:21:07 +0200

    Add java method for scanning, maybe convert to Kotlin

--- BuildingRhythms/app/src/main/java/com/example/buildingrhythms/MainActivity.kt
+++ BuildingRhythms/app/src/main/java/com/example/buildingrhythms/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.buildingrhythms
 
+import android.content.*
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import android.net.wifi.WifiManager
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
 import com.esri.arcgisruntime.geometry.Point
 import com.esri.arcgisruntime.geometry.SpatialReferences
-import com.esri.arcgisruntime.mapping.ArcGISScene
-import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource
-import com.esri.arcgisruntime.mapping.Basemap
-import com.esri.arcgisruntime.mapping.Surface
 import com.esri.arcgisruntime.mapping.view.Camera
 import com.esri.arcgisruntime.mapping.view.SceneView
+
+import com.esri.arcgisruntime.data.ServiceFeatureTable
+import com.esri.arcgisruntime.layers.ArcGISSceneLayer
+import com.esri.arcgisruntime.layers.FeatureLayer
+import com.esri.arcgisruntime.mapping.*
+import com.esri.arcgisruntime.mapping.view.MapView
 
 import com.example.buildingrhythms.databinding.ActivityMainBinding
 
@@ -56,9 +59,11 @@ class MainActivity : AppCompatActivity() {
         // Note: it is not best practice to store API keys in source code.
         // The API key is referenced here for the convenience of this tutorial.
         ArcGISRuntimeEnvironment.setApiKey("AAPKc0448a01c7d842c18fbdae5643d6ed65IgCmfZc9-fdYJPxtPca3UEp8Ft9V0zOulMBtumgxLfXxyG0wPadIluSBowA0JKCD")
+        val serviceFeatureTable = ServiceFeatureTable("https://services3.arcgis.com/jR9a3QtlDyTstZiO/arcgis/rest/services/BK_MAP_WFL1/FeatureServer/4")
+        val featureLayer = FeatureLayer(serviceFeatureTable)
 
-        val scene = ArcGISScene(Basemap.createImagery())
-
+        val scene = ArcGISScene(Basemap.createLightGrayCanvasVector())
+        scene.operationalLayers.add(featureLayer)
         // set the scene on the scene view
         sceneView.scene = scene
 
@@ -66,16 +71,19 @@ class MainActivity : AppCompatActivity() {
         val elevationSource = ArcGISTiledElevationSource("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")
         val surface = Surface(listOf(elevationSource))
         // add an exaggeration factor to increase the 3D effect of the elevation.
-        surface.elevationExaggeration = 2.5f
 
         scene.baseSurface = surface
 
         // Point(x, y, z, spatialReference)
-        val cameraLocation = Point(-118.794, 33.909, 5330.0, SpatialReferences.getWgs84())
+        val cameraLocation = Point(4.371480, 52.006719, 720.0, SpatialReferences.getWgs84())
         // Camera(location, heading, pitch, roll)
-        val camera = Camera(cameraLocation, 355.0, 72.0, 0.0)
+        val camera = Camera(cameraLocation, 355.0, 0.0, 0.0)
 
         sceneView.setViewpointCamera(camera)
+
+
+    }
+    private fun scanNetworks() {
 
     }
 
